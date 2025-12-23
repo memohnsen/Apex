e35e0cb0993f743388038bd6a9eebabb11f41566
Maddisen Mohnsen <memohnsen@gmail.com>
2025-12-23 14:37:27 -0500

    Initial Commit

---
+++ Apex/ApexApp.swift
@@ -0,0 +1,32 @@
+//
+//  ApexApp.swift
+//  Apex
+//
+//  Created by Maddisen Mohnsen on 12/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ApexApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Apex/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Apex
+//
+//  Created by Maddisen Mohnsen on 12/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
