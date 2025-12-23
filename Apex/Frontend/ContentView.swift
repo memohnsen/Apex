c71663e99266537c12509108b8165b82a1835311
Maddisen Mohnsen <memohnsen@gmail.com>
2025-12-23 17:57:25 -0500

    tabs

--- Apex/Frontend/ContentView.swift
+++ Apex/Frontend/ContentView.swift
@@ -10,15 +10,21 @@ import SwiftData
 import Foundation
 
 struct ContentView: View {
-    @State private var selectedTab: String = ""
-    @State private var apexScore = ApexScore()
+    @State private var selectedTab: String = "Home"
+    @State private var apexScore: ApexScore = ApexScore()
     
     var body: some View {
         TabView(selection: $selectedTab){
+            Tab("Home", systemImage: "house", value: "Home") {
+                HomeView()
+            }
             Tab("Calculator", systemImage: "pencil.and.list.clipboard", value: "Calculator") {
                 ScoreCalculatorView(apexScore: apexScore)
             }
             Tab("Leaderboard", systemImage: "trophy", value: "Leaderboard") {
+                LeaderboardView()
+            }
+            Tab("Standards", systemImage: "folder", value: "Standards") {
                 LeaderboardView()
             }
         }
---
+++ Apex/Frontend/Views/Tabs/Standards.swift
@@ -0,0 +1,18 @@
+//
+//  Standards.swift
+//  Apex
+//
+//  Created by Maddisen Mohnsen on 12/23/25.
+//
+
+import SwiftUI
+
+struct Standards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Standards()
+}
